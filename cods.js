694a595733039751f1ee0f385564e9927d570d2e
mocaca#3827 <93677615+TimyrTM@users.noreply.github.com>
2022-09-07 14:09:27 +0300

    test

---
+++ cods.js
@@ -0,0 +1,6 @@
+function add_to_server(){
+    document.location.href = "https://discord.com/api/oauth2/authorize?client_id=1013080169721901116&permissions=8&scope=bot%20applications.commands";
+}
+function barsik_community_server(){
+    document.location.href = "https://discord.gg/GaabpYavt2"
+}
